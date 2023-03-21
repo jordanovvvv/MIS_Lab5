07ece64533a1a7491323bc91d549f461bbffd024
jordanovvvv <jordanovsimeon1@gmail.com>
2023-03-21 13:07:24 +0100

    Added Location Services

--- lib/widgets/calendarapp.dart
+++ lib/widgets/calendarapp.dart
@@ -1,6 +1,8 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_clean_calendar/flutter_clean_calendar.dart';
+import 'package:geolocator/geolocator.dart';
+import 'package:url_launcher/url_launcher_string.dart';
 
 
 class CalendarApp extends StatefulWidget {
@@ -12,6 +14,10 @@ class _CalendarAppState extends State<CalendarApp> {
 
   DateTime? selectedDay;
   List <CleanCalendarEvent>? selectedEvent;
+
+  late String lat;
+  late String long;
+  String locationMessage = 'Current location displayed';
 
   final Map<DateTime,List<CleanCalendarEvent>> events = {
     DateTime (DateTime.now().year,DateTime.now().month,DateTime.now().day):
@@ -89,6 +95,90 @@ class _CalendarAppState extends State<CalendarApp> {
           ),
         ),
       ),
+        floatingActionButton: FloatingActionButton.extended(
+          backgroundColor: Colors.lightBlue,
+          foregroundColor: Colors.white,
+          onPressed: () {
+              getLocation();
+          },
+          icon: Icon(Icons.location_city),
+          label: Text('Get location'),
+        )
     );
   }
+  Future<Position> _getCurrentLocation() async {
+    bool serviceEnabled = await Geolocator.isLocationServiceEnabled();
+    if(!serviceEnabled){
+      return Future.error('Location services are disabled!');
+    }
+    LocationPermission permission = await Geolocator.checkPermission();
+    if(permission == LocationPermission.denied){
+      permission = await Geolocator.requestPermission();
+      if(permission == LocationPermission.denied){
+        return Future.error('Location permissions are denied!');
+      }
+    }
+    if(permission == LocationPermission.deniedForever){
+      return Future.error('Locaton permissions are permanently denied!');
+    }
+
+    return await Geolocator.getCurrentPosition();
+  }
+  void _liveLocation(){
+    LocationSettings locationSettings = const LocationSettings(
+      accuracy: LocationAccuracy.high,
+      distanceFilter: 100
+    );
+    Geolocator.getPositionStream(locationSettings: locationSettings).listen((Position position) {
+      lat = position.latitude.toString();
+      long = position.longitude.toString();
+
+      setState(() {
+        locationMessage = 'Latitude: $lat, Longitude $long';
+      });
+    });
+  }
+
+  Future<void> _openMap(String lat, String long) async {
+    String googleURL = 'https://www.google.com/maps/search/?api=1&query=$lat,$long';
+    await canLaunchUrlString(googleURL)
+    ? await launchUrlString(googleURL)
+        : throw 'Could not launch $googleURL';
+  }
+
+  Future getLocation() => showDialog(
+    context: context,
+    builder: (context) => AlertDialog(
+          title: Text('The location of the exam'),
+          content: Text(locationMessage, textAlign: TextAlign.center),
+          actions: [
+            TextButton(
+                onPressed: (){
+                  _getCurrentLocation().then((value) {
+                    lat = '${value.latitude}';
+                    long = '${value.longitude}';
+
+                    setState(() {
+                      locationMessage = 'Latitude: $lat, Longitude $long';
+                    });
+
+                    _liveLocation();
+
+                  });
+                },
+                child: Text('Get Location')),
+            TextButton(
+                onPressed: (){
+                  _openMap(lat, long);
+                },
+                child: Text('Open in Google Maps')),
+            TextButton(
+                onPressed: (){
+                    Navigator.of(context).pop();
+                },
+                child: Text('OK'))
+          ],
+      ),
+  );
+
 }
